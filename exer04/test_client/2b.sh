33806fc78808f1c9bac0b618ad1dcf3df08f113a
krispypatata <keithginoel@gmail.com>
2024-06-01 02:06:27 +0800

    fix: FIX error in executing ssh script

--- exer04/test_client/2b.sh
+++ exer04/test_client/2b.sh
@@ -4,7 +4,7 @@
 FILENAME="lab04_parallel.py"
 MATRIX_SIZE=30000
 
-for port in {5002}
+for port in {5002..5002}
 do
   gnome-terminal -- bash -c "python3 $FILENAME $MATRIX_SIZE $port 1; exec bash"
 done
--- exer04/test_host/2a.sh
+++ exer04/test_host/2a.sh
@@ -4,7 +4,7 @@
 FILENAME="lab04_parallel.py"
 MATRIX_SIZE=30000
 
-for port in {5001}
+for port in {5001..5001}
 do
   gnome-terminal -- bash -c "python3 $FILENAME $MATRIX_SIZE $port 1; exec bash"
 done
